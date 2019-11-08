232c737bc8d268d87eb7bcc1075c436c35b1d460
Sager Williams <sagerwilliams0011@gmail.com>
2019-11-08 11:49:10 -0600

    Completed Footer, Hero, and Navbar components

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Jumbotron, Container } from 'reactstrap';
+import bgimage from "";
 
 const Hero = (props) => {
-  return ( <div>Hero {props.toString()}</div> );
-}
- 
+  return (
+    <div>
+      <Jumbotron fluid style={{ backgroundImage: `url(${bgimage})`, backgroundSize: 'cover' }}>
+        <Container fluid>
+          <h1 className="display-3">{props.header}</h1>
+          <p className="lead">{props.text}</p>
+        </Container>
+      </Jumbotron>
+    </div>
+  );
+};
+
 export default Hero;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
-const Navbar = () => {
-  return ( <div>Navbar</div> );
+const Navbar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">joglogs</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="#">About</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Blog</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/login">Login</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/signup">Signup</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
 }
- 
+
 export default Navbar;
