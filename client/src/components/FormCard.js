f6c0dc2753cdb06c17bdc61ee9e697bd6a2084f1
Chris Mlinac <chrismlinac@gmail.com>
2019-11-08 13:06:31 -0600

    start on forms

--- client/src/components/FormCard.js
+++ client/src/components/FormCard.js
@@ -1,7 +1,14 @@
 import React from 'react';
-
+import { Card } from 'react-strap';
+import Form from 'Form';
 const FormCard = (props) => {
-  return ( <div>Form Card {props.toString()}</div> );
+  return (
+    <div>
+      <Card>
+        <Form type={props.formType}></Form>
+      </Card> 
+    </div>
+   );
 }
  
 export default FormCard;
--- client/src/components/Forms.js
+++ client/src/components/Forms.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { CardTitle, CardBody, CardText } from 'reactstrap';
+import { SIGN_UP, LOG_IN, START, GOAL } from '../utils/consts';
 
-const Form = (props) => {
-  return ( <div>Form {props.toString()}</div> );
+class Form extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  chooseForm(formType) {
+    switch (formType) {
+      case SIGN_UP:
+        return <SignUpForm/>
+      case LOG_IN:
+        return <LogInForm/>
+      case START:
+        return <StartForm/>
+      case GOAL:
+        return <GoalForm/>
+      default:
+        return (
+          <div>
+            <CardTitle>Error</CardTitle>
+            <CardBody>There was an issue, please try again.</CardBody>
+          </div>
+        );
+    }
+  }
+
+  render() { 
+    <div>
+      {chooseForm(this.props.formType)}
+    </div>
+  };
 }
  
 export default Form;
