49db0b575a4ce8c2580fd772a94dbafcd44916c6
Sager Williams <sagerwilliams0011@gmail.com>
2019-11-08 10:55:04 -0600

    Start to Navbar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const Navbar = () => {
-  return ( <div>Navbar</div> );
+  return ( 
+    <div>
+    <a href="/">
+      <h1>joglogs</h1>
+    </a>
+    <a href="#">
+      <h3>About</h3>
+    </a>
+    <a href="#">
+      <h3>Blog</h3>
+    </a>
+    <a href="/login">
+      <h3>Login</h3>
+    </a>
+    <a href="/signup">
+      <h3>Sign Up</h3>
+    </a>
+    </div> 
+  );
 }
  
 export default Navbar;
