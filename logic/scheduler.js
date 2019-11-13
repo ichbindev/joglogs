a3dd26b485b9453bd626d23ea9bc677d93ac8db9
Perry Williams <perrywilliams@800appliance.com>
2019-11-13 11:27:47 -0600

    scheduler.js should work with marathon26.2 choice

---
+++ logic/scheduler.js
@@ -0,0 +1,24 @@
+module.exports = scheduler.js;
+require("dotenv").config();
+require("./marathonScheduler26");
+require("./marathonScheduler13");
+require("./marathonScheduler10");
+require("./marathonScheduler5");
+
+function scheduler(data, cb) {
+  respond = cb;
+
+  if (parseFloat(data.goalDistance) === 26.2) {
+    return marathonScheduler26(data);
+  }
+  if (parseFloat(data.goalDistance) === 13.1) {
+    return marathonScheduler13(data);
+  }
+
+  if (parseFloat(data.goalDistance) === 6.2) {
+    return marathonScheduler10(data);
+  }
+  if (parseFloat(data.goalDistance) === 3.1) {
+    return marathonScheduler5(data);
+  }
+}
