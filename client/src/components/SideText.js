200909c9644374a62e4f37e4043377731d441367
Aaron Phillips <aaron.phillips929@gmail.com>
2019-11-17 13:42:53 -0600

    fixed occurence of old name

--- client/src/components/SideText.js
+++ client/src/components/SideText.js
@@ -7,12 +7,10 @@ import {
 const SideText = (props) => {
   return (
     <div>
-      
         <CardBody className="textCard">
           <CardSubtitle className="subtitle">{props.subtitle}</CardSubtitle>
           <CardText className="text">{props.text}</CardText>
         </CardBody>
-      
     </div>
   );
 };
