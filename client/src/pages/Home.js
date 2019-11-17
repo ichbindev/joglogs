7bc3c6c9005ba4cb112a30a233fa18cbac17bf56
Aaron Phillips <aaron.phillips929@gmail.com>
2019-11-17 16:31:55 -0600

    took out comment

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,34 +13,9 @@ const Home = () => {
           Instant custom training plans that seamlessly sync to your calendar
             </div>
       </div>
-      {/* <div className="rowName">
-        <img src="/media/run1.jpg" alt="run1" />
-        <div className="textDiv">
-          <div className="subtitle"><strong>Custom</strong></div>
-          <div className="subtext">Whether you're going from couch to 5k or looking to PR at your next marathon, every runner is unique and has different training needs. Train Method's algorithm creates a custom training plan to suit your fitness level and schedule.</div>
-        </div>
-      </div>
-      <div className="rowName">
-        <div className="textDiv">
-          <div className="subtitle"><strong>Instant</strong></div>
-          <div className="subtext">On the set up page, enter your details about your background and your goal.  Once your details are submitted, Train Method instantly generates a dynamic training plan for your race. Less time waiting, more time running.</div>
-        </div>
-        <img src="/media/run2.jpg" alt="run2" />
-      </div>
-      <div className="rowName">
-        <img src="/media/run3.jpg" alt="run3" />
-        <div className="textDiv">
-          <div className="subtitle"><strong>Easy to Access</strong></div>
-          <div className="subtext">Train Method easily syncs your training plan to your calendar so you don't have to go digging through spreadsheets, combing your email, or surfing the web for that training plan you found once.</div>
-        </div>
-      </div> */}
-
 <FunnelContent textFirst={false} subtitle="Custom" subtext="Whether you're going from couch to 5k or looking to PR at your next marathon, every runner is unique and has different training needs. Train Method's algorithm creates a custom training plan to suit your fitness level and schedule." img="/media/run1.jpg" alt="run1"/>
 <FunnelContent textFirst={true} subtitle="Instant" subtext="On the set up page, enter your details about your background and your goal.  Once your details are submitted, Train Method instantly generates a dynamic training plan for your race. Less time waiting, more time running." img="/media/run2.jpg" alt="run2"/>
 <FunnelContent textFirst={false} subtitle="Easy to Access" subtext="Train Method easily syncs your training plan to your calendar so you don't have to go digging through spreadsheets, combing your email, or surfing the web for that training plan you found once." img="/media/run3.jpg" alt="run3"/>
-
-
-
     </div>
   );
 }
