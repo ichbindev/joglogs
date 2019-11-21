3eeb08a5b990bce9949d4eeaf3d2f7f9161d3743
Perry Williams <perrywilliams@800appliance.com>
2019-11-21 16:06:29 -0600

    combined all the marathonschedules into one file.

--- logic/scheduler.js
+++ logic/scheduler.js
@@ -1,21 +1,24 @@
 module.exports = scheduler;
 require("dotenv").config();
-const marathonScheduler26 = require("./marathonScheduler26");
-const marathonScheduler13 = require("./marathonScheduler13");
-const marathonScheduler10 = require("./marathonScheduler10k");
-const marathonScheduler5 = require("./marathonScheduler5k");
+const marathonScheduler = require("./marathonScheduler");
+return marathonScheduler(data);
 
-function scheduler(data) {
-  if (parseFloat(data.goalDistance) === 26.2) {
-    return marathonScheduler26(data);
-  }
-  if (parseFloat(data.goalDistance) === 13.1) {
-    return marathonScheduler13(data);
-  }
-  if (parseFloat(data.goalDistance) === 6.2) {
-    return marathonScheduler10(data);
-  }
-  if (parseFloat(data.goalDistance) === 3.1) {
-    return marathonScheduler5(data);
-  }
-}
+// const marathonScheduler26 = require("./marathonScheduler26");
+// const marathonScheduler13 = require("./marathonScheduler13");
+// const marathonScheduler10 = require("./marathonScheduler10k");
+// const marathonScheduler5 = require("./marathonScheduler5k");
+//
+// function scheduler(data) {
+//   if (parseFloat(data.goalDistance) === 26.2) {
+//     return marathonScheduler26(data);
+//   }
+//   if (parseFloat(data.goalDistance) === 13.1) {
+//     return marathonScheduler13(data);
+//   }
+//   if (parseFloat(data.goalDistance) === 6.2) {
+//     return marathonScheduler10(data);
+//   }
+//   if (parseFloat(data.goalDistance) === 3.1) {
+//     return marathonScheduler5(data);
+//   }
+//  }
